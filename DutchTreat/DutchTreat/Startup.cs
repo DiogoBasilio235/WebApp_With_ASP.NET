07082ba29a9e71de347912c96e8d49ff2d807d38
diogobasilio235 <diogobasilio235@gmail.com>
2022-07-07 18:06:38 +0100

    First commit

---
+++ DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DutchTreat
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+        }
+	}
+}
+
